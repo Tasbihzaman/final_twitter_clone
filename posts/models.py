7d9987c1b38f6fd3a8280df10634240947532c4b
Krishnakishor <Krishnakishor.techis.io>
2021-11-16 13:11:40 +0530

    kk

--- posts/models.py
+++ posts/models.py
@@ -1,23 +1,33 @@
 from django.db import models
-import cloudinary
 from cloudinary.models import CloudinaryField
 
+#from django_forum.posts.views import likes
 
-# Create your models here
+
+# Create your models here.
+
+
 class Post(models.Model):
     class Meta(object):
         db_table = 'post'
 
     name = models.CharField(
-      'Name', blank=False, null=False, max_length=14, db_index=True, default='Anonymous'
-        ) 
+        'Name', blank=False, null=False, max_length=100, db_index=True, default='Anonymous'
+    )
+
     body = models.CharField(
-      'Body', blank=True, null= True,max_length=140, db_index=True
-        )
-    image = CloudinaryField("image", blank=True, db_index=True)
+        'Body', blank=True, null=True, max_length=1400, db_index=True
+    )
 
-    likes = models.PositiveIntegerField('like', default=0, db_index=True, blank=True)
+    created_at = models.DateTimeField(
+        'Created DateTime', blank=True, auto_now_add=True
+    )
 
-    created_at= models.DateTimeField( 
-       'Created DateTime ', blank=True, auto_now_add=True
-        )
+    likes = models.PositiveIntegerField(
+        'like', default=0, db_index=True, blank=True
+    )
+
+    image = CloudinaryField('image', blank= True, db_index=True)
+
+    def __str__(self):
+            return self.name
